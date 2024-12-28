0a4e5ccb12c3d4975ea126c7e6da2056cf6cf8cc
Arnabxero <66724142+MSharizal@users.noreply.github.com>
2024-12-28 20:01:56 +0800

    3rd release

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<repository-name>/', // Add this line
+  base: '/https://msharizal.github.io/OpsTenang/', // Add this line
   plugins: [react()],
 });
