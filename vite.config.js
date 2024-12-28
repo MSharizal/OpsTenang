67d36ad38142dadd7d67d8cdc90306ca82f2bf74
Arnabxero <66724142+MSharizal@users.noreply.github.com>
2024-12-28 19:54:33 +0800

    2nd release

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/<repository-name>/', // Add this line
   plugins: [react()],
-  server: {
-    port: 3000
-  }
-})
+});
